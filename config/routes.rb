ea8d765f04b2eb9e97a8fa8b1a34935502121c6c
Nkl Kgr <krueger.lup@web.de>
2023-05-26 15:26:29 +0200

    final

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
   resources :lists do
     resources :bookmarks, only: %i[new create delete]
     resources :movies, only: %i[show]
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,20 @@
 # require 'json'
 # require 'open-uri'
 
-mov_url = 'https://image.tmdb.org/t/p/w500'
-movies = URI.open('https://tmdb.lewagon.com/movie/top_rated').read
-movies_json = JSON.parse(movies)
-movies_array = movies_json["results"]
-movies_array.each do |movie|
-  poster = movie['poster_path']
-  movie = Movie.new(
-    title: movie['title'],
-    overview: movie['overview'],
-    poster_url: "https://image.tmdb.org/t/p/w500#{poster}",
-    rating: movie['vote_average']
+# mov_url = 'https://image.tmdb.org/t/p/w500'
+# movies = URI.open('https://tmdb.lewagon.com/movie/top_rated').read
+# movies_json = JSON.parse(movies)
+# movies_array = movies_json["results"]
+# movies_array.each do |movie|
+#   poster = movie['poster_path']
+#   movie = Movie.new(
+#     title: movie['title'],
+#     overview: movie['overview'],
+#     poster_url: "https://image.tmdb.org/t/p/w500#{poster}",
+#     rating: movie['vote_average']
 
-  )
-  movie.save!
-end
+#   )
+#   movie.save!
+# end
+
+List.create(name: "Classics_vol2")
