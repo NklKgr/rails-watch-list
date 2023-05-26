c99a75bc17271e00cfaab9fc73d3746778379860
Nkl Kgr <krueger.lup@web.de>
2023-05-26 15:48:07 +0200

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-# require 'json'
-# require 'open-uri'
+Movie.destroy_all
+List.destroy_all
 
-# mov_url = 'https://image.tmdb.org/t/p/w500'
-# movies = URI.open('https://tmdb.lewagon.com/movie/top_rated').read
-# movies_json = JSON.parse(movies)
-# movies_array = movies_json["results"]
-# movies_array.each do |movie|
-#   poster = movie['poster_path']
-#   movie = Movie.new(
-#     title: movie['title'],
-#     overview: movie['overview'],
-#     poster_url: "https://image.tmdb.org/t/p/w500#{poster}",
-#     rating: movie['vote_average']
 
-#   )
-#   movie.save!
-# end
+mov_url = 'https://image.tmdb.org/t/p/w500'
+movies = URI.open('https://tmdb.lewagon.com/movie/top_rated').read
+movies_json = JSON.parse(movies)
+movies_array = movies_json["results"]
+movies_array.each do |movie|
+  poster = movie['poster_path']
+  movie = Movie.new(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{poster}",
+    rating: movie['vote_average']
 
+  )
+  movie.save!
+end
+
+List.create(name: "Classics_vol1")
 List.create(name: "Classics_vol2")
